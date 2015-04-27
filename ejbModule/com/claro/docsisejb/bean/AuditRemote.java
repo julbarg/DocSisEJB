7c848bc4083c4ecbe2a223392387fcc3ef09f26c
unknown <jbarragan@G5CN332.itcol.com>
2015-04-27 10:42:52 -0500

    Resource initial commit

---
+++ ejbModule/com/claro/docsisejb/bean/AuditRemote.java
@@ -0,0 +1,12 @@
+package com.claro.docsisejb.bean;
+
+import javax.ejb.Remote;
+
+import com.claro.docsisejb.dto.DocSisDTO;
+
+@Remote
+public interface AuditRemote {
+   
+   public void auditarGeneracionDocSis(DocSisDTO docsis, String usuario);
+
+}
